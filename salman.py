e42a2c904ddf8080e6996c5f2f1f44216e7b7f7c
abbasi99 <57228131+abbasi99@users.noreply.github.com>
2019-10-31 21:35:18 +0500

    piaic
    piaic files

---
+++ salman.py
@@ -0,0 +1,19 @@
+
+sample = "salman Abbasi salman salmna salam Abbasi"
+
+
+sample_capital = sample.capitalize()
+print(sample_capital)
+
+sample_small = sample.casefold()
+print(sample_small)
+
+number = sample.count("Abbasi")
+print(number)
+
+sample_centre = sample.center(100)
+print(sample_centre)
+
+index = sample.index("salmna")
+print(index)
+
